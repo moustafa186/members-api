363f2c4bca174c49c6956c608aea680512b1f36f
Moustafa Mansour <moustafa186@gmail.com>
2019-02-22 20:45:11 +0100

    Initial commit
    Project setup

---
+++ src/main/java/com/adviqo/membersapi/MembersApiApplication.java
@@ -0,0 +1,13 @@
+package com.adviqo.membersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembersApiApplication.class, args);
+	}
+
+}
